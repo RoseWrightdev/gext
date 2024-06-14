6dba1e6cf84298342ab63f3c602a464049eabc8c
Rose Wright <rosesophiewright@gmail.com>
2024-06-13 20:45:14 -0500

    init cobra

---
+++ cmd/root.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gin_nextjs",
+	Short: "gin-nextjs is a boilerblate that combines a nextjs frontend and a gin backend",
+	Long: `gin-nextjs is a boilerblate that combines a nextjs frontend and a gin backend
+	/frontend - contains the nextjs app
+	/backend - contains the gin web server
+	
+	`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gin_nextjs.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Rose Wright <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/RoseWrightdev/gin_nextjs/cmd"
+
+func main() {
+	cmd.Execute()
+}
